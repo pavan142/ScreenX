4b455b0e5a58c0e053bc9159c73ab1e80b67eee5
pavan142 <pa1tirumani@gmail.com>
2021-03-19 17:44:22 +0530

    Replaced UsageStats with UsageEvents for detecting foreground app
    Closes #10
    
    Signed-off-by: pavan142 <pa1tirumani@gmail.com>

---
+++ app/src/main/java/com/frankenstein/screenx/helper/AppsHelper.kt
@@ -0,0 +1,59 @@
+package com.frankenstein.screenx.helper
+
+import android.app.usage.UsageEvents
+import android.app.usage.UsageStatsManager
+import com.frankenstein.screenx.Logger
+import com.frankenstein.screenx.helper.TimeHelper.Companion.getReadableTime
+import java.util.*
+import kotlin.collections.ArrayList
+
+class AppsHelper {
+    companion object {
+        private val _logger = Logger.getInstance("FILES-RECENT-APPS");
+
+        fun getRecentAppViaUsage(usm: UsageStatsManager): String {
+            var calendar = Calendar.getInstance()
+            val endTime = calendar.timeInMillis
+            calendar.add(Calendar.HOUR, -1)
+            val startTime = calendar.timeInMillis
+
+            var packageName = "";
+            val appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startTime, endTime);
+            if (appList!= null) {
+                appList.sortByDescending {it.lastTimeUsed }
+                if (appList.size > 0) {
+                    val item = appList[0];
+                    packageName = item.packageName;
+                }
+            }
+            return packageName;
+        }
+
+        private class ForeGroundAppEvent(timestamp: Long, packageName: String) {
+            val mTimestamp: Long = timestamp
+            val mPackageName: String = packageName
+        }
+
+        fun getRecentAppViaEvents(usm: UsageStatsManager): String {
+            var calendar = Calendar.getInstance()
+            val endTime = calendar.timeInMillis
+            calendar.add(Calendar.HOUR, -1)
+            val startTime = calendar.timeInMillis
+
+            var packageName = "";
+            val usageEvents = usm.queryEvents(startTime, endTime);
+            var ev: UsageEvents.Event = UsageEvents.Event();
+            val appList: ArrayList<ForeGroundAppEvent> = ArrayList()
+            while (usageEvents.getNextEvent(ev)) {
+                if (ev.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+                    packageName = ev.packageName;
+                    appList.add(ForeGroundAppEvent(ev.timeStamp, ev.packageName))
+                }
+            }
+            appList.sortByDescending { i -> i.mTimestamp }
+            if (appList.size > 0)
+                packageName = appList.get(0).mPackageName
+            return packageName
+        }
+    }
+}
